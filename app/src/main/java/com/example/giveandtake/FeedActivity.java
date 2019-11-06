3a5df4754893aa667b1bab8d43f58c850c450167
PAZJR93 <omerpaz93@gmail.com>
2019-11-06 12:25:03 +0200

    add

--- app/src/main/java/com/example/giveandtake/FeedActivity.java
+++ app/src/main/java/com/example/giveandtake/FeedActivity.java
@@ -6,7 +6,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FeedActivity extends AppCompatActivity {
-//test git naomi
+
     private TextView tvemail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
