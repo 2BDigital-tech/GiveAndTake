345051456b3954b4d725cb400f3accf9804a4404
PAZJR93 <omerpaz93@gmail.com>
2019-11-12 10:08:17 +0200

    adding buttons of option without link to user class or data base

--- app/src/main/java/com/example/giveandtake/User.java
+++ app/src/main/java/com/example/giveandtake/User.java
@@ -5,17 +5,21 @@ import java.util.ArrayList;
 
 public class User {
     public String name, email, phone;
-    public ArrayList<String> itemComputer = new ArrayList<>();
+    public ArrayList<String> Option1 = new ArrayList<>();
+    public ArrayList<String> Option2 = new ArrayList<>();
+    public ArrayList<String> Option3 = new ArrayList<>();
+    public ArrayList<String> Option4 = new ArrayList<>();
 
-    public User(){
-
-    }
-
-
-    public User(String name, String email, String phone) {
+    public User(String name, String email, String phone, ArrayList<String> option1, ArrayList<String> option2, ArrayList<String> option3, ArrayList<String> option4) {
         this.name = name;
         this.email = email;
         this.phone = phone;
-        //this.itemComputer = itemComputer;
+        Option1 = option1;
+        Option2 = option2;
+        Option3 = option3;
+        Option4 = option4;
     }
+
 }
+
+
