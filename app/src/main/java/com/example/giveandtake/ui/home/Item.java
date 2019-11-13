a3b712d2197a8ecaa2e0c23f7beb57041585c8ce
PAZJR93 <omerpaz93@gmail.com>
2019-11-13 09:05:29 +0200

    Adding The "Home" blocks with infomration on crards.

---
+++ app/src/main/java/com/example/giveandtake/ui/home/Item.java
@@ -0,0 +1,38 @@
+package com.example.giveandtake.ui.home;
+
+public class Item {
+    private int imageResocure;
+    private String NameAsk;
+    private String PhoneAsk;
+    private String Name;
+
+    public void setImageResocure(int imageResocure) {
+        this.imageResocure = imageResocure;
+    }
+
+    public void setNameAsk(String nameAsk) {
+        NameAsk = nameAsk;
+    }
+
+    public void setPhoneAsk(String phoneAsk) {
+        PhoneAsk = phoneAsk;
+    }
+
+    public int getImageResocure() {
+        return imageResocure;
+    }
+
+    public String getNameAsk() {
+        return NameAsk;
+    }
+
+    public String getPhoneAsk() {
+        return PhoneAsk;
+    }
+
+    public Item(int imageResocure, String nameAsk, String phoneAsk) {
+        this.imageResocure = imageResocure;
+        NameAsk = nameAsk;
+        PhoneAsk = phoneAsk;
+    }
+}
---
+++ app/src/main/java/com/example/giveandtake/ui/home/ItemAdapter.java
@@ -0,0 +1,66 @@
+package com.example.giveandtake.ui.home;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.giveandtake.R;
+
+import java.util.ArrayList;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+    private ArrayList<Item> Listitem;
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView _ImageView;
+        public TextView _NameAsk;
+        public TextView _PhoneAsk;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            _ImageView = itemView.findViewById(R.id.ImageView);
+            _NameAsk = itemView.findViewById(R.id.NameAsk);
+            _PhoneAsk = itemView.findViewById(R.id.PhoneAsk);
+        }
+    }
+
+    public ItemAdapter(ArrayList<Item> myListitem){
+        Listitem = myListitem;
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item,parent,false);
+        ViewHolder evh = new ViewHolder(v);
+        return evh;
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Item correntItem = Listitem.get(position);
+
+        holder._ImageView.setImageResource(correntItem.getImageResocure());
+        holder._NameAsk.setText(correntItem.getNameAsk());
+        holder._PhoneAsk.setText(correntItem.getPhoneAsk());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return Listitem.size();
+    }
+
+
+
+}
