48088b9436220bf40db10893f945854ec2be0ce7
ShimonMimoun <shimon1705@gmail.com>
2019-11-22 12:02:03 +0200

    All Post to Admin connect

--- app/src/main/java/com/example/giveandtake/Admin/AdminConnect.java
+++ app/src/main/java/com/example/giveandtake/Admin/AdminConnect.java
@@ -12,17 +12,27 @@ import com.example.giveandtake.R;
 public class AdminConnect extends AppCompatActivity {
 
     Button showuser;
+    Button showpost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_connect);
 
         showuser=(Button)findViewById(R.id.View_User);
+        showpost=(Button)findViewById(R.id.allpostadmin);
 
         showuser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent Show_User=new Intent(AdminConnect.this,ShowUsers.class);
+                startActivity(Show_User);
+            }
+        });
+
+showpost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Show_User=new Intent(AdminConnect.this,PostAdmin.class);
                 startActivity(Show_User);
             }
         });
---
+++ app/src/main/java/com/example/giveandtake/Admin/Post.java
@@ -0,0 +1,72 @@
+package com.example.giveandtake.Admin;
+
+public class Post {
+    private int imageResocure;
+    private String NameAsk;
+    private String PhoneAsk;
+    private String city;
+    private String Give;
+    private String Take;
+    private String currentUserID;
+    private String freeText;
+    private String PostId;
+
+    public void setNameAsk(String nameAsk) {
+        NameAsk = nameAsk;
+    }
+
+    public void setPhoneAsk(String phoneAsk) {
+        PhoneAsk = phoneAsk;
+    }
+
+    public void setFreeText(String newFreeText) {
+        freeText = newFreeText;
+    }
+
+
+    public int getImageResocure() {
+        return imageResocure;
+    }
+
+    public String getNameAsk() {
+        return NameAsk;
+    }
+
+    public String getPhoneAsk() {
+        return PhoneAsk;
+    }
+    public String getGive() {
+        return Give;
+    }
+    public String getTake() {
+        return Take;
+    }
+    public String getPostid() {
+        return PostId;
+    }
+    public String getCity() {
+        return city;
+    }
+
+    public String getcurrentUserID() {
+        return currentUserID;
+    }
+    public String getfreeText() {
+        return freeText;
+    }
+
+
+
+    public Post(int img ,String nameAsk, String phoneAsk,String City ,String GiveAsk, String TakeAsk,String moreInfoText ,String id, String postid) {
+        imageResocure = img;
+        NameAsk = nameAsk;
+        PhoneAsk = phoneAsk;
+        Give = GiveAsk;
+        Take = TakeAsk;
+        currentUserID = id;
+        freeText = moreInfoText;
+        PostId = postid;
+        city = City;
+
+    }
+}
