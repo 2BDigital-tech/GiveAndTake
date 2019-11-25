4a762e5cebf5eec708459caf24713de470ea6905
ShimonMimoun <shimon1705@gmail.com>
2019-11-26 01:58:41 +0200

    Addd Begin_Application

---
+++ app/src/main/java/com/android/giveandtake/Begin_Application.java
@@ -0,0 +1,14 @@
+package com.android.giveandtake;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Begin_Application extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_begin__application);
+    }
+}
