6aca614a603a9a1a115982f4e87c20469f4e94d4
ShimonMimoun <shimon1705@gmail.com>
2019-11-26 02:20:11 +0200

    Test launch application

--- app/src/main/java/com/android/giveandtake/Begin_Application.java
+++ app/src/main/java/com/android/giveandtake/Begin_Application.java
@@ -1,14 +1,43 @@
 package com.android.giveandtake;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Begin_Application extends AppCompatActivity {
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_begin__application);
+
+
+
+//if user connect as not open a login and register box
+
+        firebaseAuth=firebaseAuth.getInstance();
+
+        FirebaseUser user=firebaseAuth.getCurrentUser();
+        try{
+            Thread.sleep(1000);
+        }catch (Exception e){}
+        if(user!=null){
+
+            Intent activi=new Intent(Begin_Application.this, Connect_Fragment.class);
+            startActivity(activi);
+            finish();
+        }
+        else{
+            Intent activi2=new Intent(Begin_Application.this, Start_Application.class);
+            startActivity(activi2);
+            finish();
+
+
+        }
     }
 }
