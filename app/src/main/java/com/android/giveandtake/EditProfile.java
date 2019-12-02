83974accc9eecd30efc2b3d63c2c8e57142f51bc
NaomiO <44748207+NaomiO@users.noreply.github.com>
2019-12-02 18:31:33 +0200

    Changes in EditProfile - add the possibility to change one parameter at a time - in process

--- app/src/main/java/com/android/giveandtake/EditProfile.java
+++ app/src/main/java/com/android/giveandtake/EditProfile.java
@@ -16,7 +16,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class EditProfile extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
-    private Button save;
+    private Button save, changename, changephone, changeemail;
     private EditText newname, newphone, newemail;
     private DatabaseReference UsersRef;
     private FirebaseUser current_user;
@@ -31,15 +31,28 @@ public class EditProfile extends AppCompatActivity {
         newphone = findViewById(R.id.changephone);
         newemail = findViewById(R.id.changemail);
 
+
         firebaseAuth = firebaseAuth.getInstance();
 
         save = (Button)findViewById(R.id.savebtn);
+        changename = (Button)findViewById(R.id.namebutton);
+        changephone = (Button)findViewById(R.id.phonebutton);
+        changeemail = (Button)findViewById(R.id.emailbutton);
         mySpinner = (Spinner) findViewById(R.id.cityspinner);
 
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(EditProfile.this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.City));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
+
+
+        changename.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String changedname = newname.getText().toString();
+            }
+        });
+
 
 
         save.setOnClickListener(new View.OnClickListener() {
