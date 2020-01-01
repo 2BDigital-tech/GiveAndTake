15a34213dc17a33bb7aa47a6954006034ad070ea
ShimonMimoun <shimon1705@gmail.com>
2020-01-01 11:31:42 +0200

    Dell Notification

--- app/src/main/java/com/android/giveandtake/Start_Application.java
+++ app/src/main/java/com/android/giveandtake/Start_Application.java
@@ -37,6 +37,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 /**
  * This class represents the starting page of the application where you can register to the app or log in if you're alreagy registered
